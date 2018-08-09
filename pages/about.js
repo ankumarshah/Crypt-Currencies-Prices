31e950ebc65fd0a7b8325de204aa57ef67c58d75
Anand Shah <anandshah.shah@gmail.com>
2018-08-10 00:11:57 +0530

    adding crypto currencies

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+    <div>
+        <Layout />
+        <h1>About BitzPrice</h1>
+        <p>Application to View Bitcoin Prices</p>
+    </div>
+    );
+    
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout';
+import Prices from '../components/Prices';
+
+const Index = (props) => (
+    <div>
+        <Layout />
+        <h1>Welcome To BitzPrice</h1>
+        <p>Check current Bitcoin rate</p>
+        <Prices bpi={props.bpi}/>
+    </div>
+);
+Index.getInitialProps = async function() {
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json();
+
+    return {
+        bpi: data.bpi
+    }
+}
+export default Index;
